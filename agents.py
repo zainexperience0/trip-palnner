f3255c764380649855c2cc3600b54d0cc783e25e
zainexperience0 <zainexperience0@gmail.com>
2024-05-31 09:08:37 +0500

    runing locally

--- agents.py
+++ agents.py
@@ -35,9 +35,8 @@ Notes:
 
 class TravelAgents:
     def __init__(self):
-        self.OpenAIGPT35 = ChatOpenAI(
-            model_name="gpt-3.5-turbo", temperature=0.7)
-        self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
+        self.llm = ChatOpenAI(
+            model_name="crewai-llama3-8b", api_key="NA", base_url="http://localhost:11434/v1")
 
     def expert_travel_agent(self):
         return Agent(
@@ -54,7 +53,7 @@ class TravelAgents:
                 CalculatorTools.calculate
             ],
             verbose=True,
-            llm=self.OpenAIGPT4,
+            llm=self.llm,
         )
 
     def city_selection_expert(self):
@@ -66,7 +65,7 @@ class TravelAgents:
                 f"""Select the best cities based on weather, season, prices, and traveler interests"""),
             tools=[SearchTools.search_internet],
             verbose=True,
-            llm=self.OpenAIGPT4,
+            llm=self.llm,
         )
 
     def local_tour_guide(self):
@@ -78,5 +77,5 @@ class TravelAgents:
                 f"""Provide the BEST insights about the selected city"""),
             tools=[SearchTools.search_internet],
             verbose=True,
-            llm=self.OpenAIGPT4,
+            llm=self.llm,
         )
